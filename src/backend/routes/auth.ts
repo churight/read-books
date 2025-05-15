cb2007d3e99c4652da8e5433a716c8d68d684362
churight <kchuright@gmaol.com>
2025-05-15 14:23:20 +0300

    im tired of this pipeline

--- src/backend/routes/auth.ts
+++ src/backend/routes/auth.ts
@@ -68,7 +68,7 @@ router.post('/login', async (req, res) =>{
 
 //profile
 
-router.get('/profile',protect, async (req: express.Request & { user?: any }, res) =>{
+router.get('/profile',protect, async (req: express.Request & { user?: any }, res: Response) =>{
     try{
         const user = await User.findById(req.user?._id).select('-password');
         if(!user) return res.status(404).json({message: "User not found"});
